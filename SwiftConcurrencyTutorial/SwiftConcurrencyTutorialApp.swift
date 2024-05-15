b9dd5af57381b91cf4d9ce3249c9a635a37810d7
Enes UTKU <ensutku@hotmail.com>
2024-05-15 15:51:57 +0300

    Initial Commit

---
+++ SwiftConcurrencyTutorial/SwiftConcurrencyTutorialApp.swift
@@ -0,0 +1,12 @@
+// Created by Enes UTKU
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
